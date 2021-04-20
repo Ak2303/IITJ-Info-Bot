8547139fd19287bc580fd915369f75c187921958
Aklovya Gupta <ak2305.2001@gmail.com>
2021-04-20 18:17:55 +0530

    Added msg Layout

---
+++ app/src/main/java/com/example/iitjinfobot/ChatWindowActivity.java
@@ -0,0 +1,14 @@
+package com.example.iitjinfobot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChatWindowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_window);
+    }
+}
